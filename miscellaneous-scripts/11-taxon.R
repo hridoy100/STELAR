d02b8bd8e8ee3cf8142077fe9696f3df45397410
Mazharul Islam <mazhar.buet11@gmail.com>
2020-02-20 10:43:05 -0500

    Update 11-taxon.R

--- miscellaneous-scripts/11-taxon.R
+++ miscellaneous-scripts/11-taxon.R
@@ -6,7 +6,7 @@ data <- read.csv(file="11-taxon.csv", header=TRUE, sep=",")
 print(data)
 genes = c(5,15,25,50,100)
 modelconditions = c("true", "estimated")
-methods = c("astral","stelar","mpest","SuperTriplets")
+methods = c("ASTRAL","STELAR","MPEST","SuperTriplets")
 
 
 Mean = c()
@@ -19,14 +19,14 @@ for (gene in genes) {
   for( modelcondition in modelconditions) {
     for(method in methods) {
         mydata = data[data$method == method & data$modelCondition == modelcondition & data$gene == gene,]
-        
+
         Mean = c(Mean, mean(mydata$fn))
         Sd = c(Sd, sd(mydata$fn/sqrt(length(mydata$fn))))
-        
+
         Gene = c(Gene, gene)
         if (grepl("SuperTriplets", method, fixed = TRUE) == FALSE) {
           Method = c(Method, toupper(method))
-        } 
+        }
         else {
           Method = c(Method, method)
         }
@@ -36,7 +36,7 @@ for (gene in genes) {
 }
 print(Mean)
 data = data.frame(
-  "mean" = Mean, 
+  "mean" = Mean,
   "sd" = Sd,
   "gene" = Gene,
   "modelcondition" = Modelcondition,
@@ -51,7 +51,7 @@ p =  p + theme(axis.text.y = element_text(size = 12, face = "bold"))
 p = p + geom_errorbar(aes(ymin=data$mean-data$sd, ymax=data$mean+data$sd),
                       width=0.3,                    # Width of the error bars
                       position=position_dodge(.9))
-p = p + ylab("Avg. Robinson-Foulds distance") 
+p = p + ylab("Avg. Robinson-Foulds distance")
 p = p + xlab("number of genes")
 p  = p + facet_grid(cols = vars(data$modelcondition)) + theme(strip.text.x = element_text(size = 12, face="bold"))
 p =  p + theme(legend.position="bottom", legend.title=element_blank())
@@ -59,4 +59,3 @@ p =  p + theme(legend.position="bottom", legend.title=element_blank())
 # axis.text.x=element_blank(),
 #axis.ticks.x=element_blank())
 p
-
