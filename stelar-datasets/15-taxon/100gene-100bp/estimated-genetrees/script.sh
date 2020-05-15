6ae395c85fd900653a10a6290dd318b74380bc7c
Mazharul Islam <mazhar.buet11@gmail.com>
2020-05-14 20:36:26 -0400

    Removed dataset

--- stelar-datasets/15-taxon/100gene-100bp/estimated-genetrees/script.sh
+++
@@ -1,9 +0,0 @@
- #!/usr/bin/bash
-for i in {01..10}
-do
-        rm truegenetrees*
-        cp "true-gene-trees/"$i truegenetrees1
-        ./mpest  ./truegenetrees1  ./specieslist15 1
-        cp truegenetrees1.best.of.1.tre "true-gene-trees/"$i".mpest"
-        rm control*
-done
